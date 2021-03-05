3bfc4c90a8193ec700323690f6e5a284e457ae75
Michael Farris <spacecowby069@yahoo.com>
2021-03-05 17:15:19 -0500

    My Final Commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "BurgerTime", "Star Wars", "Ms. Pac Man", "Donkey Kong", "Asteroids", "Tron" };
+
+            var allGames = games.Where(x => x.Length > 0).OrderByDescending(x => x.Length);
+
+            foreach (var i in allGames)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
